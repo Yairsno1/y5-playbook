a958cf6a1aafa1ccc37bf06a8d8c845e73ec11ad
yairsno1 <yairsno1@gmail.com>
2020-12-24 10:06:35 +0200

    Play logic completed

--- playbook/play.go
+++ playbook/play.go
@@ -19,15 +19,10 @@ func NewPlay(name, desc string, intialSetup Setting) *Play {
 	return &rv
 }
 
-//Add an action of O to the play according to order.
-func (play *Play) Add(order int, threat ...IAction) {
-	//TODO: order validation
-	iPhase := len(play.phases) - 1
-	phase := play.phases[iPhase]
-
-	for _, t := range threat {
-		phase.addThreat(t, order)
-	}
+//Completed indicates if the play is done and can't be changed.
+func (play *Play) Completed() bool {
+	count := play.PhaseCount()
+	return play.phases[count-1].active
 }
 
 //CurrPhase is a getter for the current(last) phase of the play.
@@ -41,4 +36,58 @@ func (play Play) CurrPhase() Phase {
 //PhaseCount indicates the play's number of phases.
 func (play Play) PhaseCount() int {
 	return len(play.phases)
+}
+
+//Add an action of O to the play according to order.
+func (play *Play) Add(order int, threat ...IAction) {
+	//TODO: order validation
+	iPhase := len(play.phases) - 1
+	phase := play.phases[iPhase]
+
+	for _, t := range threat {
+		phase.addThreat(t, order)
+	}
+}
+
+//Done blocks the play to accept changes.
+//Call to Rollback to unblock.
+func (play *Play) Done() {
+	count := play.PhaseCount()
+	play.phases[count-1].active = false
+}
+
+//GetPhase gets phase number phaseNum.
+func (play Play) GetPhase(phaseNum int) Phase {
+	//TODO: validate index
+	rv := play.phases[phaseNum-1]
+
+	return *rv
+}
+
+//GoOn completes the current phase and creates the next phase
+func (play *Play) GoOn() {
+	iPhase := len(play.phases) - 1
+	curr := play.phases[iPhase]
+	curr.active = false
+
+	set := curr.start
+	for _, order := range curr.actions {
+		for _, act := range order {
+			set = act.Do(set)
+		}
+	}
+	play.phases = append(play.phases, newPhase(set))
+}
+
+//Rollback rewinds the play to phase number phaseNum
+func (play *Play) Rollback(phaseNum int) {
+	count := play.PhaseCount()
+	//TODO: validate index
+
+	if phaseNum < count {
+		play.phases = play.phases[:phaseNum]
+	}
+
+	count = play.PhaseCount()
+	play.phases[count-1].active = true
 }
